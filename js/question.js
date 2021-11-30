07e008c50399e448378648172656899a3b8d8467
RubenRuaro <ruben.ruaro300700@gmail.com>
2021-11-29 18:31:59 -0600

    Commit inicial

---
+++ js/question.js
@@ -0,0 +1,36 @@
+//Arreglo de Objetos
+
+const quiz = [
+  {
+    q: "It _____ a beautiful day today",
+    options: ["is", "are", "am"],
+    answer: 0,
+  },
+  {
+    q: "What are you doing right now?",
+    options: [
+      "You are studying english",
+      "He is studying english",
+      "I am studying english",
+      "We are studying english",
+    ],
+    answer: 2,
+  },
+  {
+    q: "Choose the word that defines the following image.",
+    options: ["Radio", "Camera", "Phone", "Computer"],
+    answer: 3,
+    img: "img/pc.png",
+  },
+  {
+    q: "What time is it?",
+    options: ["Its one fifty", "Its one fifty-five", "Its ten past eleven"],
+    answer: 1,
+    img: "img/reloj.jpg",
+  },
+  {
+    q: '"be," "have" y "do" son verbos auxiliares?',
+    options: ["True", "False"],
+    answer: 0,
+  },
+];
